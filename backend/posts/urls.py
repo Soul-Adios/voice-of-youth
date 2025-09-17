b736f6e147526b280cf03cec7a8304dc6ecfab0a
Soul-Adios <ardhraajith82@gmail.com>
2025-09-17 13:55:45 +0530

    files removed

--- backend/posts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PostView, UpvoteView
-
-urlpatterns = [
-    path("posts/", PostView.as_view(), name="post-list"),
-    path("upvote/<int:post_id>/", UpvoteView.as_view(), name="post-upvote"),
-]
